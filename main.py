ce1e049b520bb0cecce0af1f43b2ced2bf99a136
Nathan M <nathan.mimoun@gmail.com>
2019-07-11 14:00:32 +0200

    Establishing new file as module

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("salut toi !")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import module.MacGyver as mc
+
+def main():
+    mc.MacGyver.main()
+
+if __name__ == "__main__":
+    print("execution du programme principale")
+    main()
+
+else:
+    print("non non ce n'est pas un module !")
---
+++ module/MacGyver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class MacGyver:
+    def __init(self):
+        print("voici le héro :)")
+    def main():
+        print("voici le hero :) ")
+
+if __name__ == "__main__":
+    main()
