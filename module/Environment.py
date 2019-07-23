30375c9f6d9309b863cffac190cbe112c14077b0
Nathan M <nathan.mimoun@gmail.com>
2019-07-24 01:13:17 +0200

    Correction de la class et mise à jour des fonctions (main() fonctionne)

--- module/Environment.py
+++ module/Environment.py
@@ -3,72 +3,74 @@
 
 import random
 
-level = [
-"xxxxxxxxxxxxxxx",
-"x    xxx    xxx",
-"xxx  xxx    xxx",
-"xxx  xxx    xxx",
-"xxx  xxx    xxx",
-"xxx  xxx    xxx",
-"xxx  xxx    xxx",
-"xxx  xxx    xxx",
-"xxxxxxxxxxxxxxx"]
-
-
-level1 = [
-"xxxxxxxxxxxxxxx",
-"x    xxx    xxx",
-"x    xxx    xxx",
-"x    xxx    xxx",
-"x    xxx    xxx",
-"x    xxx    xxx",
-"x    xxx    xxx",
-"x    xxx    xxx",
-"xxxxxxxxxxxxxxx"]
-
 
 class Environment:
 
-    def __init__ (self):
+    def __init__ (self,needle, ether, tube, guardian, wall_coord, road_coord):
 
     #loading the labyrinth file
-        needle = [0,0] #setting the 1st object with null coordinate
-        ether = [0,0] #setting the 2nd object with null coordinate
-        tube = [0,0] #setting the 3rd object with null coordinate
-        MAX_WIDTH = 16
-        MAX_LENGHT = 16
-        guardian = [0,0] #setting the gard variable with the exit of lab position
+        self.needle = needle #setting the 1st object with null coordinate
+        self.ether = ether #setting the 2nd object with null coordinate
+        self.tube = tube #setting the 3rd object with null coordinate
+        self.guardian = guardian #setting the gard variable with the exit of lab position
+        self.wall_coord = wall_coord
+        self.road_coord = road_coord
 
-    def lab_coord (self):
+    def lab_coord (self, level):
         ### Fonction that translate draw maze into coordinate ###
-        wall_coord= []
-        road_coord= []
-
-        for y in range(len(self)):
-            for x in range(len(self[y])):
+        self.level = level
+        for y in range(len(self.level)):
+            for x in range(len(self.level[y])):
                 painting = level [y][x]
                 #print(x,y)
                 if painting == "x":
                     print("/", end="")
-                    wall_coord.append([x,y])
+                    self.wall_coord.append([x,y])
+                elif painting == "g":
+                    print("g", end="")
+                    self.guardian.append([x,y])
                 else:
                     print("@", end="")
-                    road_coord.append([x,y])
-                ### --- ajouter une condition qui repers la position du gardien --- ###
+                    self.road_coord.append([x,y])
             print()
-        return road_coord
-        #print(wall_coord)
 
-    #def rand_position (self, wall):
+    def rand_position (self, road_coord):
         ### Fonction that randomly place an object in the maze ###
-        object_coord = random.sample(road_coord, k=3)
-        #print(object_coord)
-        needle = object_coord[0]
-        ether = object_coord[1]
-        tube = object_coord[2]
-        print("voici needle", needle)
-        print("voici ether", ether)
-        print("voici tube", tube)
+        object_coord = random.sample(self.road_coord, k=3)
+        self.needle = object_coord[0]
+        self.ether = object_coord[1]
+        self.tube = object_coord[2]
 
-    def main():
-        Environment.lab_coord(level1)
+def main():
+    print("voici le niveau :) ")
+    needle = []
+    ether = []
+    tube = []
+    guardian = []
+    wall_coord = []
+    road_coord = []
+    runloop = True
+    level = [
+    "xxxxxxxxxxgxxxx",
+    "x    xxx    xxx",
+    "xxx  xxx    xxx",
+    "xxx  xxx    xxx",
+    "xxx  xxx    xxx",
+    "xxx  xxx    xxx",
+    "xxx  xxx    xxx",
+    "xxx  xxx    xxx",
+    "xxxxxxxxxxxxxxx"]
+    content = Environment(needle, ether, tube, guardian, wall_coord, road_coord)
+    etat = 1
+    while runloop == True and etat > 0:
+        content.lab_coord(level)
+        content.rand_position(wall_coord)
+        print("voici la position de l'aiguille {}".format(content.needle))
+        print("voici la position du gardien {}".format(content.guardian))
+        etat -= 1
+    else:
+        print("fin")
+
+
+if __name__ == "__main__":
+    main()
