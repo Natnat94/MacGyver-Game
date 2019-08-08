d0523d1e417b0122c82c493ff5f5b059074694ce
Nathan M <nathan.mimoun@gmail.com>
2019-08-08 18:05:44 +0200

    le jeu affiche le lab en command que lorsque mode command choisis

--- module/Environment.py
+++ module/Environment.py
@@ -11,9 +11,9 @@ class Environment:
         self.guardian = [] #setting the guardian variable with null coordinate
         self.wall_coord = [] #setting the wall variable with null coordinate
         self.road_coord = [] #setting the roads variable with null coordinate
-        self.mac_position = [] #setting the hero variable with null coordinate
+        self.mac_position = None #setting the hero variable with null coordinate
         self.objects = []
-        self.level = ""
+        self.level = None
         self.names = ["ether.png", "needle.png", "tube.png"]
 
     def read_file(self, file_to_read):
@@ -21,25 +21,25 @@ class Environment:
         with open(file_to_read, "r") as text:
             self.level = text.read().splitlines()
 
-    def lab_coord(self):
+    def lab_coord(self, command = False):
         """Fonction that translate draw maze into coordinate"""
         for i in range(len(self.level)):
             for j in range(len(self.level[i])):
-                painting = self.level[i][j] # changer a partir de ici pour que ce soit fit to scale
+                painting = self.level[i][j]
                 if painting == "x":
-                    print("/", end="")
+                    if command: print("/", end="")
                     self.wall_coord.append([(j-1)*40, (i-1)*40])
                 elif painting == "g":
-                    print("g", end="")
+                    if command: print("g", end="")
                     self.guardian = [(j-1)*40, (i-1)*40]
                 elif painting == "m":
-                    print("m", end="")
+                    if command: print("m", end="")
                     self.mac_position = [(j-1)*40, (i-1)*40]
                 else:
-                    print("@", end="")
+                    if command: print("@", end="")
                     self.road_coord.append([(j-1)*40, (i-1)*40])
-            print()
-            
+            if command: print()
+
     def rand_position(self):
         """Fonction that randomly place an object in the maze"""
         object_coord = random.sample(self.road_coord, k=3)
